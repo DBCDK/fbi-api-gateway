fc4a82c475e18705ca0e17416203f48302180893
pjo <pjo@dbc.dk>
2021-04-28 14:10:46 +0200

    autoformat

--- src/schema/root.js
+++ src/schema/root.js
@@ -3,8 +3,8 @@
  *
  */
 
-import {log} from 'dbc-node-logger';
-import {createHistogram} from '../utils/monitor';
+import { log } from "dbc-node-logger";
+import { createHistogram } from "../utils/monitor";
 
 /**
  * The root type definitions
@@ -32,29 +32,29 @@ type Mutation {
 export const resolvers = {
   Query: {
     manifestation(parent, args, context, info) {
-      return {id: args.pid};
+      return { id: args.pid };
     },
     monitor(parent, args, context, info) {
       try {
         context.monitorName = args.name;
         createHistogram(args.name);
-        return 'OK';
+        return "OK";
       } catch (e) {
         return e.message;
       }
     },
     async help(parent, args, context, info) {
-      return {q: args.q};
+      return { q: args.q };
     },
     async user(parent, args, context, info) {
       return { ...args };
     },
     async work(parent, args, context, info) {
-      const {work} = await context.datasources.workservice.load(args.id);
-      return {...work, id: args.id};
+      const { work } = await context.datasources.workservice.load(args.id);
+      return { ...work, id: args.id };
     },
     async search(parent, args, context, info) {
-      return {q: args.q, limit: args.limit, offset: args.offset};
+      return { q: args.q, limit: args.limit, offset: args.offset };
     },
     async library(parent, args, context, info) {
       return {
@@ -64,34 +64,35 @@ export const resolvers = {
       };
     },
     async suggest(parent, args, context, info) {
-      return {q: args.q};
+      return { q: args.q };
     },
-
   },
   Mutation: {
     data_collect(parent, args, context, info) {
       // Check that exactly one input type is given
       const inputObjects = Object.values(args.input);
       if (inputObjects.length !== 1) {
-        throw new Error('Exactly 1 input must be specified');
+        throw new Error("Exactly 1 input must be specified");
       }
 
       // Convert keys, replace _ to -
       const data = {};
       Object.entries(inputObjects[0]).forEach(([key, val]) => {
-        data[key.replace(/_/g, '-')] = val;
+        data[key.replace(/_/g, "-")] = val;
       });
 
       // We log the object, setting 'type: "data"' on the root level
       // of the log entry. In this way the data will be collected
       // by the AI data collector
-      log.info('data', {type: 'data', message: JSON.stringify(data)});
+      log.info("data", { type: "data", message: JSON.stringify(data) });
 
-      return 'OK';
+      return "OK";
     },
     async submitOrder(parent, args, context, info) {
-      return await context.datasources.submitOrder.load(
-          {input: args.input, accessToken: context.accessToken});
+      return await context.datasources.submitOrder.load({
+        input: args.input,
+        accessToken: context.accessToken,
+      });
     },
   },
 };
