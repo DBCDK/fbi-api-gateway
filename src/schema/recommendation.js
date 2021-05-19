5be673ddc681033639638c83830708fe8ec8fad4
pjo <pjo@dbc.dk>
2021-05-19 12:43:38 +0200

    kfu added some things for data_collect

--- src/schema/recommendation.js
+++ src/schema/recommendation.js
@@ -1,6 +1,8 @@
 export const typeDef = `
 type Recommendation {
+  work: Work!
   manifestation: WorkManifestation!
+  reader: String
   value: Float
 }`;
 
@@ -11,6 +13,10 @@ export const resolvers = {
     },
     manifestation(parent) {
       return { id: parent.pid };
+    },
+    async work(parent, args, context, info) {
+      const res = await context.datasources.workservice.load(parent.work);
+      return { ...res, id: args.id };
     },
   },
 };
