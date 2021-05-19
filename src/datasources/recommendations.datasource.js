32f6033be816bc98ec5b7239d88e91ba6435f614
pjo <pjo@dbc.dk>
2021-05-19 11:59:05 +0200

    new recommender

--- src/datasources/recommendations.datasource.js
+++ src/datasources/recommendations.datasource.js
@@ -6,13 +6,18 @@ import request from "superagent";
 
 export async function load({ pid, limit = 10 }) {
   return (
-    await request
-      .post("http://recompass-work-1-2.mi-prod.svc.cloud.dbc.dk/recompass-work")
-      .send({
-        likes: [pid],
-        limit,
-      })
-  ).body;
+    (
+      await request
+        //.post("http://recompass-work-1-2.mi-prod.svc.cloud.dbc.dk/recompass-work")
+        .post("http://booklens-1-1.mi-prod.svc.cloud.dbc.dk")
+        .send({
+          like: [pid],
+          agencies: [190101],
+          persistent_work: true,
+          limit: limit,
+        })
+    ).body
+  );
 }
 
 export const options = {
