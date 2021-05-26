83a6d8d34cc23d9b138163d2d2f9052742d83414
pjo <pjo@dbc.dk>
2021-05-26 12:02:54 +0200

    pickupbranch as arg

--- src/schema/checkorder.js
+++ src/schema/checkorder.js
@@ -14,18 +14,6 @@ export const typeDef = `
     orderPossibleReason: OrderPossibleReason
    }`;
 
-/*
-@TODO
-ENUM from
-<xs:enumeration value="owned_accepted"/>
-<xs:enumeration value="not_owned_ILL_loc"/>
-<xs:enumeration value="owned_wrong_mediumType"/>
-<xs:enumeration value="not_owned_wrong_ILL_mediumType"/>
-<xs:enumeration value="not_owned_no_ILL_loc"/>
-<xs:enumeration value="owned_own_catalogue"/>
-<xs:enumeration value="not_owned_accepted_by_consortia"/>
- */
-
 export const resolvers = {
   CheckOrderPolicy: {
     lookUpUrl(parent, args, context, info) {
