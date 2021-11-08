aecd808544c2c94b7ddc3a4eb9bb9ede680e9cb0
pjo <pjo@dbc.dk>
2021-11-08 10:36:27 +0100

    error in soap - fix

--- src/datasources/localizations.datasource.js
+++ src/datasources/localizations.datasource.js
@@ -10,22 +10,22 @@ const { url, ttl, prefix } = config.datasources.holdingstatus;
  */
 function constructSoap(pids) {
   const soappids = pids.map((pid) => `<ns1:pid>${pid}</ns1:pid>`).join("");
+
   let soap = `<?xml version="1.0" encoding="UTF-8"?>
   <SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ns1="http://oss.dbc.dk/ns/openholdingstatus"><SOAP-ENV:Body>
     <ns1:localisationsRequest>
         <ns1:agencyId>DK-870970</ns1:agencyId>
         ${soappids}
-        <ns1:mergePids>true</ns1:mergePids>
         <ns1:outputType>json</ns1:outputType>
         <ns1:mergePids>true</ns1:mergePids>
     </ns1:localisationsRequest>
   </SOAP-ENV:Body></SOAP-ENV:Envelope>`;
+
   return soap;
 }
 
 function parseResponse(text) {
   const obj = JSON.parse(text);
-
   const count =
     (obj.localisationsResponse &&
       obj.localisationsResponse.localisations[0] &&
