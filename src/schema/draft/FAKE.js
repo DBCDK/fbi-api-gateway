74810122df3c9502eefa266ceb08779591d93f27
pjo <pjo@dbc.dk>
2022-06-20 10:47:01 +0200

    prefix dummydata with ... DUMMY

--- src/schema/draft/FAKE.js
+++ src/schema/draft/FAKE.js
@@ -1,26 +1,26 @@
 export const FAKE_GENERAL_SERIES = {
-  title: "Some Series",
+  title: "Dummy Some Series",
   parallelTitles: [
-    "Some Series, parallel title",
-    "Some Series, another parallel title",
+    "Dummy  Some Series, parallel title",
+    "Dummy Some Series, another parallel title",
   ],
   alternativeTitles: [],
   numberInSeries: {
-    display: "number one",
+    display: "Dummy number one",
     number: 1,
   },
   isPopular: false,
 };
 
 export const FAKE_POPULAR_SERIES = {
-  title: "Some Series",
+  title: "Dummy Some Series",
   alternativeTitles: [
-    "Some Series, parallel title",
-    "Some Series, another parallel title",
+    "Dummy Some Series, parallel title",
+    "Dummy Some Series, another parallel title",
   ],
   parallelTitles: [],
   numberInSeries: {
-    display: "number one",
+    display: "Dummy number one",
     number: 1,
   },
   isPopular: true,
@@ -29,47 +29,47 @@ export const FAKE_POPULAR_SERIES = {
 };
 
 export const FAKE_TRANSLATION = {
-  singular: "forfatter",
-  plural: "forfattere",
+  singular: "Dummy forfatter",
+  plural: "Dummy forfattere",
 };
 
 export const FAKE_ROLE = {
-  functionCode: "aut",
+  functionCode: "Dummy aut",
   function: FAKE_TRANSLATION,
 };
 
 export const FAKE_PERSON = {
   __typename: "Person",
-  display: "Jens Jensen",
-  nameSort: "Jensen Jens",
-  firstName: "Jens",
-  lastName: "Jensen",
-  birthYear: "1950",
-  romanNumeral: "Jens Jensen IV",
-  attributeToName: "Jens Jensen, testperson",
+  display: "Dummy Jens Jensen",
+  nameSort: "Dummy Jensen Jens",
+  firstName: "Dummy Jens",
+  lastName: "Dummy Jensen",
+  birthYear: "Dummy 1950",
+  romanNumeral: "Dummy Jens Jensen IV",
+  attributeToName: "Dummy Jens Jensen, testperson",
   aliases: [
-    { display: "Svend Svendsen, personen bag Jens Jensen" },
-    { display: "Kirsten Kirstensen, personen bag Jens Jensen" },
+    { display: "Dummy Svend Svendsen, personen bag Jens Jensen" },
+    { display: "Dummy Kirsten Kirstensen, personen bag Jens Jensen" },
   ],
   roles: [FAKE_ROLE],
 };
 
 export const FAKE_ACCESS = {
   __typename: "AccessUrl",
-  origin: "DBC Webarkiv",
-  url: "https://moreinfo.dbc.dk",
+  origin: "Dummy DBC Webarkiv",
+  url: "Dummy https://moreinfo.dbc.dk",
 };
 
 export const FAKE_CORPORATION = {
   __typename: "Corporation",
-  display: "Some Corporation",
-  nameSort: "Some Corporation for sorting",
-  main: "Some Corporation",
-  sub: "Some Sub Corporation",
-  location: "Some location",
-  year: "1950",
-  number: "5",
-  attributeToName: "Some Corporation ...",
+  display: "Dummy Some Corporation",
+  nameSort: "Dummy Some Corporation for sorting",
+  main: "Dummy Some Corporation",
+  sub: "Dummy Some Sub Corporation",
+  location: "Dummy Some location",
+  year: "Dummy 1950",
+  number: "Dummy 5",
+  attributeToName: "Dummy Some Corporation ...",
   roles: [],
 };
 
@@ -77,13 +77,13 @@ export const FAKE_SUBJECTS = {
   all: [
     {
       __typename: "SubjectText",
-      type: "TOPIC",
-      display: "Some fictional subject",
+      type: "Dummy TOPIC",
+      display: "Dummy Some fictional subject",
     },
     {
       __typename: "TimePeriod",
-      display: "1950-1980",
-      period: { begin: 1950, end: 1980, display: "1950-1980" },
+      display: "Dummy 1950-1980",
+      period: { begin: 1950, end: 1980, display: "Dummy 1950-1980" },
     },
     FAKE_PERSON,
     FAKE_CORPORATION,
@@ -91,13 +91,13 @@ export const FAKE_SUBJECTS = {
   dbcVerified: [
     {
       __typename: "SubjectText",
-      type: "TOPIC",
-      display: "Some fictional subject",
+      type: "Dummy TOPIC",
+      display: "Dummy Some fictional subject",
     },
     {
       __typename: "TimePeriod",
-      display: "1950-1980",
-      period: { begin: 1950, end: 1980, display: "1950-1980" },
+      display: "Dummy 1950-1980",
+      period: { begin: 1950, end: 1980, display: "Dummy 1950-1980" },
     },
     FAKE_PERSON,
     FAKE_CORPORATION,
@@ -105,157 +105,157 @@ export const FAKE_SUBJECTS = {
 };
 
 export const FAKE_MANIFESTATION_TITLE = {
-  main: ["Some Title"],
-  full: ["Some Title: Full"],
-  alternative: ["Some Title: Alternative"],
-  identifyingAddition: "Indlæst af Jens Jensen",
-  original: ["Some Title: Original"],
-  parallel: ["Parallel Title 1", "Parallel Title 2"],
-  sort: "Some Title Sort",
-  standard: "Some Title Standard",
-  translated: ["En Oversat Titel"],
+  main: ["Dummy Some Title"],
+  full: ["Dummy Some Title: Full"],
+  alternative: ["Dummy Some Title: Alternative"],
+  identifyingAddition: "Dummy Indlæst af Jens Jensen",
+  original: ["Dummy Some Title: Original"],
+  parallel: ["Dummy Parallel Title 1", "Parallel Title 2"],
+  sort: "Dummy Some Title Sort",
+  standard: "Dummy Some Title Standard",
+  translated: ["Dummy En Oversat Titel"],
 };
 
 export const FAKE_AUDIENCE = {
-  generalAudience: ["general audience"],
-  ages: [{ display: "10-14", begin: 10, end: 14 }],
-  libraryRecommendation: "some library recommendation",
-  childrenOrAdults: [{ display: "til børn", code: "FOR_CHILDREN" }],
-  schoolUse: [{ display: "til skolebrug", code: "FOR_SCHOOL_USE" }],
-  primaryTarget: ["Some primary target"],
-  let: "some let",
-  lix: "some lix",
+  generalAudience: ["Dummy general audience"],
+  ages: [{ display: "Dummy 10-14", begin: 10, end: 14 }],
+  libraryRecommendation: "Dummy some library recommendation",
+  childrenOrAdults: [{ display: "Dummy til børn", code: "Dummy FOR_CHILDREN" }],
+  schoolUse: [{ display: "Dummy til skolebrug", code: "Dummy FOR_SCHOOL_USE" }],
+  primaryTarget: ["Dummy Some primary target"],
+  let: "Dummy some let",
+  lix: "Dummy some lix",
 };
 
 export const FAKE_CLASSIFICATION = {
-  system: "DK5",
-  code: "86-096",
-  display: "Skønlitteratur",
-  entryType: "NATIONAL_BIBLIOGRAPHY_ENTRY",
+  system: "Dummy DK5",
+  code: "Dummy 86-096",
+  display: "Dummy Skønlitteratur",
+  entryType: "Dummy NATIONAL_BIBLIOGRAPHY_ENTRY",
 };
 
 export const FAKE_EDITION = {
-  summary: "3. i.e. 2 udgave, 2005",
-  edition: "3. i.e. 2 udgave",
+  summary: "Dummy 3. i.e. 2 udgave, 2005",
+  edition: "Dummy 3. i.e. 2 udgave",
   contributors: [],
   publicationYear: {
-    display: "2005",
+    display: "Dummy 2005",
     year: 2005,
   },
 };
 
 export const FAKE_LATEST_PRINTING = {
-  summary: "11. oplag, 2020",
-  printing: "11. oplag",
+  summary: "Dummy 11. oplag, 2020",
+  printing: "Dummy 11. oplag",
   publicationYear: {
-    display: "2020",
+    display: "Dummy 2020",
     year: 2020,
   },
 };
 
 export const FAKE_HOST_PUBLICATION = {
-  title: "Årsskrift / Carlsbergfondet",
-  creator: "Some Creator",
-  isbn: "some isbn",
-  issue: "some issue",
-  notes: ["a note"],
-  pages: "140-145",
-  publisher: "Some Publisher",
-  summary: "Årsskrift / Carlsbergfondet, 2006",
-  issn: "1395-7961",
+  title: "Dummy Årsskrift / Carlsbergfondet",
+  creator: "Dummy Some Creator",
+  isbn: "Dummy some isbn",
+  issue: "Dummy some issue",
+  notes: ["Dummy a note"],
+  pages: "Dummy 140-145",
+  publisher: "Dummy Some Publisher",
+  summary: "Dummy Årsskrift / Carlsbergfondet, 2006",
+  issn: "Dummy 1395-7961",
   year: {
-    display: "2006",
+    display: "Dummy 2006",
     year: 2006,
   },
   series: FAKE_GENERAL_SERIES,
 };
 
 export const FAKE_LANGUAGES = {
-  main: [{ display: "dansk", isoCode: "dan" }],
-  original: [{ display: "dansk", isoCode: "dan" }],
-  parallel: [{ display: "dansk", isoCode: "dan" }],
-  spoken: [{ display: "dansk", isoCode: "dan" }],
-  subtitles: [{ display: "dansk", isoCode: "dan" }],
-  abstract: [{ display: "dansk", isoCode: "dan" }],
+  main: [{ display: "Dummy dansk", isoCode: "Dummy dan" }],
+  original: [{ display: "Dummy dansk", isoCode: "Dummy dan" }],
+  parallel: [{ display: "Dummy dansk", isoCode: "Dummy dan" }],
+  spoken: [{ display: "Dummy dansk", isoCode: "Dummy dan" }],
+  subtitles: [{ display: "Dummy dansk", isoCode: "Dummy dan" }],
+  abstract: [{ display: "Dummy dansk", isoCode: "Dummy dan" }],
 };
 
 export const FAKE_MANIFESTATION_PARTS = {
-  type: "MUSIC_TRACKS",
-  heading: "Indhold:",
+  type: "Dummy MUSIC_TRACKS",
+  heading: "Dummy Indhold:",
   parts: [
     {
-      title: "Bouquet royal",
+      title: "Dummy Bouquet royal",
       creators: [
         {
           __typename: "Person",
-          display: "H. C. Lumbye",
-          nameSort: "Lumbye, H.C.",
-          firstName: "H. C.",
-          lastName: "Lumbye",
+          display: "Dummy H. C. Lumbye",
+          nameSort: "Dummy Lumbye, H.C.",
+          firstName: "Dummy H. C.",
+          lastName: "Dummy Lumbye",
           roles: [],
         },
       ],
-      creatorsFromDescription: ["arr.: Peter Ettrup Larsen"],
+      creatorsFromDescription: ["Dummy arr.: Peter Ettrup Larsen"],
       classifications: [
         {
-          system: "DK5",
-          code: "78.424",
-          display: "Klaver og strygere. Orgel og strygere",
+          system: "Dummy DK5",
+          code: "Dummy 78.424",
+          display: "Dummy Klaver og strygere. Orgel og strygere",
         },
       ],
     },
   ],
 };
 
-export const FAKE_MATERIALTYPE = { general: "bøger", specific: "bog" };
+export const FAKE_MATERIALTYPE = { general: "Dummy bøger", specific: "Dummy bog" };
 
 export const FAKE_NOTES = {
-  type: "NOT_SPECIFIED",
-  display: ["Indspillet i Ꜳrhus Musikhus 12.-14. juni 2020"],
+  type: "Dummy NOT_SPECIFIED",
+  display: ["Dummy Indspillet i Ꜳrhus Musikhus 12.-14. juni 2020"],
 };
 
 export const FAKE_PHYSICALDESCRIPTION = {
   summary:
-    "1 dvd-rom Xbox One Nødvendigt udstyr Xbox One. - Med multiplayerfunktion Spiludvikler fremgår ikke af materialet",
-  extent: "1 dvd-rom",
-  requirements: "Nødvendigt udstyr Xbox One. - Med multiplayerfunktion",
-  technicalInformation: "Xbox One",
+    "Dummy 1 dvd-rom Xbox One Nødvendigt udstyr Xbox One. - Med multiplayerfunktion Spiludvikler fremgår ikke af materialet",
+  extent: "Dummy 1 dvd-rom",
+  requirements: "Dummy Nødvendigt udstyr Xbox One. - Med multiplayerfunktion",
+  technicalInformation: "Dummy Xbox One",
 };
 
 export const FAKE_PUBLICATIONYEAR = {
-  display: "1839",
+  display: "Dummy 1839",
   year: 1839,
-  frequency: "ugentlig",
+  frequency: "Dummy ugentlig",
 };
 
 export const FAKE_SHELFMARK = {
-  prefix: "some prefix",
-  shelfmark: "some shelfmark",
+  prefix: "Dummy some prefix",
+  shelfmark: "Dummy some shelfmark",
 };
 
 export const FAKE_LIST_OF_CONTENT = {
-  heading: "Indhold",
+  heading: "Dummy Indhold",
   listOfContent: [
     {
-      heading: "Puderne",
-      listOfContent: [{ content: "Bruddet" }, { content: "Hustelefonen" }],
+      heading: "Dummy Puderne",
+      listOfContent: [{ content: "Dummy Bruddet" }, { content: "Dummy Hustelefonen" }],
     },
     {
-      content: "Tykke-Olsen m.fl.",
+      content: "Dummy Tykke-Olsen m.fl.",
     },
     {
-      content: "Over skulderen",
+      content: "Dummy Over skulderen",
     },
   ],
 };
 
 export const FAKE_MANIFESTATION_1 = {
-  pid: "some-pid-1",
+  pid: "Dummy some-pid-1",
   titles: FAKE_MANIFESTATION_TITLE,
-  abstract: ["Some abstract ..."],
+  abstract: ["Dummy Some abstract ..."],
   accessTypes: [
-    { display: "fysisk", code: "PHYSICAL" },
-    { display: "online", code: "ONLINE" },
+    { display: "Dummy fysisk", code: "Dummy PHYSICAL" },
+    { display: "Dummy online", code: "Dummy ONLINE" },
   ],
   access: FAKE_ACCESS,
   audience: FAKE_AUDIENCE,
@@ -264,28 +264,28 @@ export const FAKE_MANIFESTATION_1 = {
       ...FAKE_PERSON,
       roles: [
         {
-          functionCode: "ill",
+          functionCode: "Dummy ill",
           function: {
-            singular: "illustrator",
-            plural: "illustratorer",
+            singular: "Dummy illustrator",
+            plural: "Dummy illustratorer",
           },
         },
       ],
     },
   ],
-  contributorsFromDescription: ["på dansk ved Vivi Berendt"],
+  contributorsFromDescription: ["Dummy på dansk ved Vivi Berendt"],
   creators: [FAKE_PERSON, FAKE_CORPORATION],
-  creatorsFromDescription: ["tekst af William Warren"],
+  creatorsFromDescription: ["Dummy tekst af William Warren"],
   classifications: [FAKE_CLASSIFICATION],
   edition: FAKE_EDITION,
   latestPrinting: FAKE_LATEST_PRINTING,
-  fictionNonfiction: { display: "skønlitteratur", code: "FICTION" },
-  genreAndForm: ["some genre"],
+  fictionNonfiction: { display: "Dummy skønlitteratur", code: "Dummy FICTION" },
+  genreAndForm: ["Dummy some genre"],
   hostPublication: FAKE_HOST_PUBLICATION,
   identifiers: [
     {
-      type: "ISBN",
-      value: "1234567891234",
+      type: "Dummy ISBN",
+      value: "Dummy 1234567891234",
     },
   ],
   languages: FAKE_LANGUAGES,
@@ -294,50 +294,50 @@ export const FAKE_MANIFESTATION_1 = {
   notes: [FAKE_NOTES],
   relatedPublications: [
     {
-      heading: "Tidligere titel:",
-      title: ["Yngre læger"],
-      issn: "0105-0508",
+      heading: "Dummy Tidligere titel:",
+      title: ["Dummy Yngre læger"],
+      issn: "Dummy 0105-0508",
     },
     {
-      heading: "Udgave i andet medium: Også på cd-rom",
-      title: ["Ugeskrift for læger"],
-      issn: "1399-4174",
+      heading: "Dummy Udgave i andet medium: Også på cd-rom",
+      title: ["Dummy Ugeskrift for læger"],
+      issn: "Dummy 1399-4174",
     },
   ],
   physicalDescriptions: [FAKE_PHYSICALDESCRIPTION],
   publicationYear: FAKE_PUBLICATIONYEAR,
-  publisher: ["Lægeforeningen"],
-  recordCreationDate: "19830414",
+  publisher: ["Dummy Lægeforeningen"],
+  recordCreationDate: "Dummy 19830414",
   series: [FAKE_POPULAR_SERIES, FAKE_GENERAL_SERIES],
   shelfmark: FAKE_SHELFMARK,
-  source: ["some source"],
+  source: ["Dummy some source"],
   subjects: FAKE_SUBJECTS,
-  volume: "Bind 2",
+  volume: "Dummy Bind 2",
   tableOfContents: FAKE_LIST_OF_CONTENT,
 };
 
 export const FAKE_MANIFESTATION_2 = {
   ...FAKE_MANIFESTATION_1,
-  pid: "some-pid-2",
+  pid: "Dummy some-pid-2",
 };
 
 export const hest = {
-  fisk: "hest",
+  fisk: "Dummy hest",
 };
 
 export const FAKE_WORKTITLES = {
-  main: ["Some Title"],
-  full: ["Some Title: Full"],
-  parallel: ["Parallel Title 1", "Parallel Title 2"],
-  sort: "Some Title Sort",
-  original: ["Some Title Origintal"],
-  standard: "Some Title Standard",
-  translated: ["Oversat titel"],
+  main: ["Dummy Some Title"],
+  full: ["Dummy Some Title: Full"],
+  parallel: ["Dummy Parallel Title 1", "Dummy Parallel Title 2"],
+  sort: "Dummy Some Title Sort",
+  original: ["Dummy Some Title Origintal"],
+  standard: "Dummy Some Title Standard",
+  translated: ["Dummy Oversat titel"],
 };
 
 export const FAKE_DK5MAINENTRY = {
-  display: "some dk5 display",
-  code: "some dk5 code",
+  display: "Dummy some dk5 display",
+  code: "Dummy some dk5 code",
 };
 
 export const FAKE_WORK = {
@@ -345,17 +345,17 @@ export const FAKE_WORK = {
   titles: {
     FAKE_WORKTITLES,
   },
-  abstract: ["The abstract"],
+  abstract: ["Dummy The abstract"],
   creators: [FAKE_PERSON, FAKE_CORPORATION],
   dk5MainEntry: FAKE_DK5MAINENTRY,
-  fictionNonfiction: { display: "skønlitteratur", code: "FICTION" },
+  fictionNonfiction: { display: "Dummy skønlitteratur", code: "Dummy FICTION" },
   materialTypes: FAKE_MATERIALTYPE,
   series: [FAKE_POPULAR_SERIES, FAKE_GENERAL_SERIES],
-  universe: { title: "Some Universe" },
-  genreAndForm: ["some genre"],
-  workTypes: ["LITERATURE"],
-  workYear: "1950",
-  mainLanguages: [{ display: "dansk", isoCode: "dan" }],
+  universe: { title: "Dummy Some Universe" },
+  genreAndForm: ["Dummy some genre"],
+  workTypes: ["Dummy LITERATURE"],
+  workYear: "Dummy 1950",
+  mainLanguages: [{ display: "Dummy dansk", isoCode: "Dummy dan" }],
   subjects: FAKE_SUBJECTS,
   manifestations: {
     first: FAKE_MANIFESTATION_1,
@@ -366,8 +366,8 @@ export const FAKE_WORK = {
 
 export const FAKE_SUGGEST_RESPONSE = {
   result: [
-    { type: "title", term: "Some Title", work: FAKE_WORK },
-    { type: "creator", term: "Some Creator", work: FAKE_WORK },
+    { type: "Dummy title", term: "Dummy Some Title", work: FAKE_WORK },
+    { type: "Dummy creator", term: "Dummy Some Creator", work: FAKE_WORK },
   ],
 };
 
