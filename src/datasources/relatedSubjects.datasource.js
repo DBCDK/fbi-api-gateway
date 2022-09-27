e8bb1dfe77f2b5c69ac35243ad650422bf48cea5
pjo <pjo@dbc.dk>
2022-09-27 12:56:39 +0200

    ampersand

--- src/datasources/relatedSubjects.datasource.js
+++ src/datasources/relatedSubjects.datasource.js
@@ -1,8 +1,6 @@
 import config from "../config";
 
 export async function load({ q, limit }, context) {
-  console.log(q);
-
   const url = config.datasources.relatedsubjects.url;
   let query = "?q=" + q.join("&=");
   if (limit) {
