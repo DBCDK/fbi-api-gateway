ef8c1a5f4e6d0bf247c6215771dc52798e588406
Kasper Fuglsang <kfu@dbc.dk>
2022-10-17 14:09:45 +0200

    added localRecommend + example

--- src/schema/root.js
+++ src/schema/root.js
@@ -67,6 +67,11 @@ type Query {
   """
   recommend(id: String, pid: String, faust: String, limit: Int): RecommendationResponse!
 
+  """
+  Get local recommendations for specific branch
+  """
+  localRecommend(id: String, pid: String, faust: String, limit: Int, branchId: String!): RecommendationResponse!
+
 
   help(q: String!, language: LanguageCode): HelpResponse
   branches(agencyid: String, branchId: String, language: LanguageCode, q: String, offset: Int, limit: PaginationLimit, status: LibraryStatus, bibdkExcludeBranches:Boolean): BranchResult!
@@ -255,6 +260,9 @@ export const resolvers = {
       return args;
     },
     recommend(parent, args, context, info) {
+      return args;
+    },
+    localRecommend(parent, args, context, info) {
       return args;
     },
     async deleteOrder(parent, args, context, info) {
