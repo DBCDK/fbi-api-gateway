d44f2b3c999aa8f5df93fdcff49db1491545aa51
mtni <mtni@dbc.dk>
2022-10-31 12:29:45 +0100

    ...

--- src/schema/category.js
+++ src/schema/category.js
@@ -12,30 +12,143 @@ export const typeDef = `
   }
   
   type Categories {
-    childrenBooksNonfiction: [Category!]!
-    childrenBooksFiction: [Category!]!
-    fiction: [Category!]!
-    nonfiction: [Category!]!
-    eBooks: [Category!]!
-    articles: [Category!]!
-    movies: [Category!]!
-    games: [Category!]!
-    music: [Category!]!
-    sheetMusic: [Category!]!
+    childrenBooksNonfiction(
+      """
+      Filter on categories title e.g. "nyeste"
+      """
+      filters: [String!]
+    ): [Category!]!
+
+    childrenBooksFiction(
+      """
+      Filter on categories title e.g. "nyeste"
+      """
+      filters: [String!]
+    ): [Category!]!
+
+    fiction(
+      """
+      Filter on categories title e.g. "nyeste"
+      """
+      filters: [String!]
+    ): [Category!]!
+
+    nonfiction(
+      """
+      Filter on categories title e.g. "nyeste"
+      """
+      filters: [String!]
+    ): [Category!]!
+
+    eBooks(
+      """
+      Filter on categories title e.g. "nyeste"
+      """
+      filters: [String!]
+    ): [Category!]!
+
+    articles(
+      """
+      Filter on categories title e.g. "nyeste"
+      """
+      filters: [String!]
+    ): [Category!]!
+
+    movies(
+      """
+      Filter on categories title e.g. "nyeste"
+      """
+      filters: [String!]
+    ): [Category!]!
+
+    games(
+      """
+      Filter on categories title e.g. "nyeste"
+      """
+      filters: [String!]
+    ): [Category!]!
+
+    music(
+      """
+      Filter on categories title e.g. "nyeste"
+      """
+      filters: [String!]
+    ): [Category!]!
+
+    sheetMusic(
+      """
+      Filter on categories title e.g. "nyeste"
+      """
+      filters: [String!]
+    ): [Category!]!
  }`;
+
+function filterCategories(categories, filters) {
+  return filters
+    .map((f) => categories.find((c) => c.title === f))
+    .filter((f) => f);
+}
 
 export const resolvers = {
   Categories: {
-    childrenBooksNonfiction(parent) {
+    childrenBooksNonfiction(parent, args) {
+      if (args.filters?.length > 0) {
+        return filterCategories(parent.childrenbooks_nonfiction, args.filters);
+      }
       return parent.childrenbooks_nonfiction;
     },
-    childrenBooksFiction(parent) {
+    childrenBooksFiction(parent, args) {
+      if (args.filters?.length > 0) {
+        return filterCategories(parent.childrenbooks_fiction, args.filters);
+      }
       return parent.childrenbooks_fiction;
     },
-    eBooks(parent) {
+    fiction(parent, args) {
+      if (args.filters?.length > 0) {
+        return filterCategories(parent.fiction, args.filters);
+      }
+      return parent.fiction;
+    },
+    nonfiction(parent, args) {
+      if (args.filters?.length > 0) {
+        return filterCategories(parent.nonfiction, args.filters);
+      }
+      return parent.nonfiction;
+    },
+    eBooks(parent, args) {
+      if (args.filters?.length > 0) {
+        return filterCategories(parent.ebooks, args.filters);
+      }
       return parent.ebooks;
     },
-    sheetMusic(parent) {
+    articles(parent, args) {
+      if (args.filters?.length > 0) {
+        return filterCategories(parent.articles, args.filters);
+      }
+      return parent.articles;
+    },
+    movies(parent, args) {
+      if (args.filters?.length > 0) {
+        return filterCategories(parent.movies, args.filters);
+      }
+      return parent.movies;
+    },
+    games(parent, args) {
+      if (args.filters?.length > 0) {
+        return filterCategories(parent.games, args.filters);
+      }
+      return parent.games;
+    },
+    music(parent, args) {
+      if (args.filters?.length > 0) {
+        return filterCategories(parent.music, args.filters);
+      }
+      return parent.music;
+    },
+    sheetMusic(parent, args) {
+      if (args.filters?.length > 0) {
+        return filterCategories(parent.sheet_music, args.filters);
+      }
       return parent.sheet_music;
     },
   },
