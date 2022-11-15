d540503def447fe8a73220fe1d4e999166cc8492
pjo <pjo@dbc.dk>
2022-11-15 10:12:47 +0100

    enable redis

--- src/datasources/moreinfoCovers.datasource.js
+++ src/datasources/moreinfoCovers.datasource.js
@@ -127,10 +127,10 @@ export async function status(loadFunc) {
   }
 }
 
-/*export const options = {
+export const options = {
   redis: {
     prefix,
     ttl,
     staleWhileRevalidate: 60 * 60 * 24 * 90, // 90 days
   },
-};*/
+};
