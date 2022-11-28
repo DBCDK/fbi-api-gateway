2cbfa0d024e1ff5f26062cd53bd99b60bbde7714
pjo <pjo@dbc.dk>
2022-11-28 13:14:23 +0100

    tiny timeout on default forsider

--- src/config.js
+++ src/config.js
@@ -32,8 +32,8 @@ export default {
       url:
         process.env.DEFAULTFORSIDER_URL ||
         "http://default-forsider.febib-staging.svc.cloud.dbc.dk/",
-      ttl: process.env.DEFAULTFORSIDER_TIME_TO_LIVE_SECONDS || 60 * 60 * 24,
-      prefix: process.env.DEFAULTFORSIDER_PREFIX || "defaultforsider-1",
+      ttl: process.env.DEFAULTFORSIDER_TIME_TO_LIVE_SECONDS || 30,
+      prefix: process.env.DEFAULTFORSIDER_PREFIX || "defaultforsider-2",
     },
     relatedsubjects: {
       url:
