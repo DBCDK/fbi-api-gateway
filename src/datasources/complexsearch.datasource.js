e8ee797c7de247a85e789eddba4631f03d60d101
pjo <pjo@dbc.dk>
2023-01-05 13:28:00 +0100

    added profile  on complex search

--- src/datasources/complexsearch.datasource.js
+++ src/datasources/complexsearch.datasource.js
@@ -7,12 +7,13 @@ const { url, ttl, prefix } = config.datasources.complexsearch;
  */
 export async function load({ cql, offset, limit, profile }, context) {
   // TODO service needs to support profile ...
-
   const res = await context?.fetch(`${url}/cqlquery`, {
     method: "POST",
     body: JSON.stringify({
       cqlQuery: cql,
       pagination: { offset, limit },
+      agency: profile.agency,
+      profile: profile.name,
     }),
     allowedErrorStatusCodes: [400],
   });
