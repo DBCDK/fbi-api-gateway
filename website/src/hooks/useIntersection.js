525a3d91cc9fb8ac36c5d78e2597ac29895ccf0c
kfudbc <kfudbc@users.noreply.github.com>
2023-02-27 09:40:16 +0100

    fix stuff (#103)

--- website/src/hooks/useIntersection.js
+++ website/src/hooks/useIntersection.js
@@ -1,15 +1,17 @@
 import { useState, useEffect } from "react";
+import debounce from "lodash/debounce";
 
 export default function useIntersection(element, rootMargin) {
   const [isVisible, setState] = useState(false);
 
   useEffect(() => {
-    const observer = new IntersectionObserver(
-      ([entry]) => {
-        setState(entry.isIntersecting);
-      },
-      { rootMargin }
-    );
+    const handleObservation = debounce(function (entries) {
+      setState(entries.some((entry) => entry.isIntersecting));
+    }, 100);
+
+    const observer = new IntersectionObserver(handleObservation, {
+      rootMargin,
+    });
 
     element && observer.observe(element);
 
