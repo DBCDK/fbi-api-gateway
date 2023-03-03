d44d639132fb574d15300f9855f49fc84b450a67
Kasper Fuglsang <kfu@dbc.dk>
2023-03-03 13:45:26 +0100

    integrate with service

---
+++ src/datasources/intelligentFacets.datasource.js
@@ -0,0 +1,50 @@
+/**
+ * @file Facets
+ */
+
+import config from "../config";
+
+const {
+  url,
+  prefix,
+  ttl,
+  token,
+  firstHits,
+  disableFuzzySearch,
+} = config.datasources.facets;
+
+export async function load({ q, filters = {}, profile, limit = 10 }, context) {
+  const { agency, name } = profile;
+  // get parsed arguments for query
+  // static parameters for the search
+  const statics = {
+    "access-token": token,
+    debug: false,
+    agency,
+    "facets.first_hits": firstHits,
+    profile: name,
+    "facets.intelligent_facet": true,
+  };
+
+  // merge variables and statics
+  const query = {
+    q,
+    filters,
+    "facet.limit": limit,
+    disable_fuzzy_search: disableFuzzySearch,
+    ...statics,
+  };
+
+  const res = (
+    await context.fetch(url, { method: "POST", body: JSON.stringify(query) })
+  ).body;
+
+  return res;
+}
+
+export const options = {
+  redis: {
+    prefix,
+    ttl,
+  },
+};
