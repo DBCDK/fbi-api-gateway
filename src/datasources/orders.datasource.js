e035330d8ca8dc48845968ee398a6e6ab6c45996
pjohans <pjohans@gmail.com>
2023-05-11 13:36:39 +0200

    fixed order datasource

--- src/datasources/orders.datasource.js
+++ src/datasources/orders.datasource.js
@@ -4,16 +4,22 @@ import config from "../config";
 /**
  * Fetch user info
  */
-export async function load({ accessToken }, context) {
-  const url = config.datasources.openplatform.url + "/user";
-
-  return (
-    await context.fetch(url, {
-      method: "POST",
-      body: JSON.stringify({
-        access_token: accessToken,
-        userinfo: ["userOrder"],
-      }),
-    })
-  ).body.data;
+export async function load({accessToken}, context) {
+    const url = config.datasources.openplatform.url + "/user";
+    return (
+        await request.post(url).send({
+            access_token: accessToken,
+            userinfo: ["userOrder"],
+        })
+    ).body.data;
+    /*const fisk =
+        await context.fetch(url, {
+            method: "POST",
+            body: JSON.stringify({
+                access_token: accessToken,
+                userinfo: ["userOrder"],
+            }),
+        })
+    console.log(fisk, "FISK")
+    return fisk.body.data;*/
 }
