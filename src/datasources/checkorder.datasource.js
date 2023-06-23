a5a50fd2d1518706b11ed045b84c10d7176e1e8b
pjohans <pjohans@gmail.com>
2023-06-23 08:41:26 +0200

    new checkorder endpoint

--- src/datasources/checkorder.datasource.js
+++ src/datasources/checkorder.datasource.js
@@ -1,100 +1,22 @@
-/**
- * @file Get suggestions
- *
- * This is temporary until real suggester is implemented
- */
+const url =
+  "http://copa-rs.iscrum-ors-staging.svc.cloud.dbc.dk/copa-rs/api/v1/checkorderpolicy/";
+const serviceRequester = "190101";
 
-import request from "superagent";
-import config from "../config";
+export async function load({ pid, pickupBranch, accessToken }, context) {
+  const post = {
+    pickUpAgencyId: pickupBranch,
+    pid: [pid],
+    serviceRequester: serviceRequester,
+  };
 
-let parseString = require("xml2js").parseString;
-let stripNS = require("xml2js").processors.stripPrefix;
+  const policy = await context.fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+    body: JSON.stringify(post),
+  });
 
-const {
-  authenticationUser,
-  authenticationGroup,
-  authenticationPassword,
-  serviceRequester,
-  url,
-  ttl,
-  prefix,
-} = config.datasources.openorder;
-
-function createRequest(pid, pickupAgency) {
-  return `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:open="http://oss.dbc.dk/ns/openorder">
-    <soapenv:Header/>
-    <soapenv:Body>
-<open:checkOrderPolicyRequest xmlns:open="http://oss.dbc.dk/ns/openorder">
-  <open:authentication>
-                <open:groupIdAut>${authenticationGroup}</open:groupIdAut>
-                <open:passwordAut>${authenticationPassword}</open:passwordAut>
-                <open:userIdAut>${authenticationUser}</open:userIdAut>
-            </open:authentication>
-            <open:pickUpAgencyId>${pickupAgency}</open:pickUpAgencyId>
-            <!--Zero or more repetitions:-->
-            <open:pid>${pid}</open:pid>
-            <open:serviceRequester>${serviceRequester}</open:serviceRequester>
-</open:checkOrderPolicyRequest>
-</soapenv:Body>
-</soapenv:Envelope>`;
+  return policy?.body || null;
 }
-
-export async function load({ pid, pickupBranch }) {
-  const xml = createRequest(pid, pickupBranch);
-  const policy = await request
-    .post(url)
-    .set("Content-Type", "text/xml")
-    .send(xml)
-    .then((body) => {
-      parseString(
-        body.text,
-        { trim: true, tagNameProcessors: [stripNS] },
-        function (err, result) {
-          body = result.Envelope.Body[0].checkOrderPolicyResponse[0];
-        }
-      );
-      const data = {};
-
-      if (body.checkOrderPolicyError) {
-        // @TODO log
-        return {
-          statusCode: 500,
-          error: body.checkOrderPolicyError[0],
-        };
-      }
-
-      if (body.orderPossible) {
-        data.orderPossible = body.orderPossible[0] !== "false";
-      }
-      if (body.orderPossibleReason) {
-        data.orderPossibleReason = body.orderPossibleReason[0];
-      }
-      if (body.lookUpUrl) {
-        data.lookUpUrl = body.lookUpUrl[0];
-      }
-      return { statusCode: 200, data: data };
-    })
-    .catch((err) => {
-      // @TODO log
-      console.log(err);
-    });
-
-  return policy.data;
-}
-
-/**
- * The status function
- *
- * @throws Will throw error if service is down
- */
-
-// export async function status(loadFunc) {
-//   await loadFunc({pid: "870970-basis:51877330", pickupBranch: "710100"});
-// }
-
-export const options = {
-  redis: {
-    prefix,
-    ttl,
-  },
-};
