effc06b141ba1f2fe4c8346c4a7ad9a547f4dd0b
AoschkA <jonaspraem@gmail.com>
2023-07-11 15:10:46 +0200

    Update user.datasource.js

--- src/datasources/user.datasource.js
+++ src/datasources/user.datasource.js
@@ -1,9 +1,55 @@
 import config from "../config";
 
+const { url: openuserstatusUrl } = config.datasources.openuserstatus;
+const {
+  authenticationUser,
+  authenticationGroup,
+  authenticationPassword,
+} = config.datasources.openorder;
+
+const constructSoap = ({ agencyId, userId }) => {
+  let soap = `
+<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:open="http://oss.dbc.dk/ns/openuserstatus">
+  <soapenv:Header/>
+  <soapenv:Body>
+    <open:getUserStatusRequest>
+      <open:agencyId>${agencyId}</open:agencyId>
+      <open:authentication>
+        <open:groupIdAut>${authenticationGroup}</open:groupIdAut>
+        <open:passwordAut>${authenticationPassword}</open:passwordAut>
+        <open:userIdAut>${authenticationUser}</open:userIdAut>
+      </open:authentication>
+      <open:outputType>json</open:outputType>
+      <open:userId>${userId}</open:userId>
+      <open:selectUserInfo>userData</open:selectUserInfo>
+    </open:getUserStatusRequest>
+  </soapenv:Body>
+</soapenv:Envelope>
+`;
+
+  return soap;
+};
+
+const reduceBody = (body) => ({
+});
+
 /**
  * Fetch user info
  */
 export async function load({ accessToken }, context) {
+  const soap = constructSoap({ agencyId: "726500", userId: "2904951253" });
+  const res2 = await context?.fetch(openuserstatusUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "text/xml",
+    },
+    body: soap,
+  });
+
+  const data = res2.body;
+  console.log("USER", data);
+
+  /*  */
   const url = config.datasources.openplatform.url + "/user";
 
   const res = await context?.fetch(url, {
