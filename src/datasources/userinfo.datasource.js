2f13ef28432e80e83bed403241ff2a14d0ce03a2
mtni <mtni@dbc.dk>
2023-11-15 12:51:07 +0100

    ...

--- src/datasources/userinfo.datasource.js
+++ src/datasources/userinfo.datasource.js
@@ -6,10 +6,24 @@ const { url, ttl, prefix } = config.datasources.userInfo;
  * Fetch user info
  */
 export async function load({ accessToken }, context) {
-  const res = await context?.fetch(url, {
+  const res = await context?.fetch(`${url}?skipBorchk=1`, {
     headers: { Authorization: `Bearer ${accessToken}` },
     allowedErrorStatusCodes: [401],
   });
+
+  // TODO: Remove agency backfill from smaug, when userinfo supports loggedInAgencyId
+  if (res.body?.attributes) {
+    const smaug = await context.getLoader("smaug").load({
+      accessToken,
+    });
+
+    return {
+      attributes: {
+        ...res.body.attributes,
+        loggedInAgencyId: smaug?.user?.agency || null,
+      },
+    };
+  }
 
   return res.body;
 }
@@ -22,13 +36,14 @@ export async function testLoad({ accessToken }, context) {
   const loginAgency = testUser?.loginAgency;
   return {
     attributes: {
-      userId: loginAgency.cpr || loginAgency.localId,
+      userId: loginAgency?.cpr || loginAgency?.localId,
       blocked: false,
       uniqueId: loginAgency?.uniqueId,
       agencies: accountsToCulr(testUser.merged),
       municipalityAgencyId: testUser.merged.find(
         (account) => account.isMunicipality
       )?.agency,
+      loggedInAgencyId: loginAgency?.agencyId,
     },
   };
 }
