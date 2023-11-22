faa4a641f32e86681dfdcd1aa258150504ec538a
Yousef Mohsen <yousefmohsen@outlook.dk>
2023-11-22 09:18:42 +0100

    submit multiple orders workid

--- src/schema/order.js
+++ src/schema/order.js
@@ -453,8 +453,12 @@ export const resolvers = {
             return;
           }
           successfullyCreated.push(material.key);
-          //todo fix
-        //  await saveOrderToUserdata(context, "test 2 submitOrderRes multiple");
+          console.log('\n\n\n\nMMULTIPLE ORDRS.material',material)
+          console.log('\n\n\n\nMMULTIPLE ORDRS.submitOrderRes',submitOrderRes)
+          console.log('\n\n\n\nMMULTIPLE ORDRS.args',args)
+
+          const pidToOrder = material.pids[0];
+          await saveOrderToUserdata({context, submitOrderRes, pid:pidToOrder});
         })
       );
 
