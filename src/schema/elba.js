5d183c64b18b6d431a2977978a287631ce489729
kiscdbc <kisc@dbc.dk>
2023-12-05 13:39:45 +0100

    reuse placeCopyRequest in multiorder

--- src/schema/elba.js
+++ src/schema/elba.js
@@ -69,103 +69,107 @@ export const resolvers = {
   },
   ElbaServices: {
     async placeCopyRequest(parent, args, context, info) {
-      const { pid, userName, userMail } = args.input;
-
-      // token is not authenticated
-      if (!context?.user?.userId) {
-        return {
-          status: "ERROR_UNAUTHENTICATED_USER",
-        };
-      }
-
-      const originRequester =
-        context?.smaug?.digitalArticleService?.originRequester;
-
-      if (!originRequester) {
-        return {
-          status: "ERROR_MISSING_CLIENT_CONFIGURATION",
-        };
-      }
-
-      // Basic user information (e.g. name, email)
-      const account = filterAgenciesByProps(context?.user?.agencies, {
-        type: "CPR",
-      })?.[0];
-
-      const userData =
-        (await context.datasources.getLoader("user").load({
-          userId: account?.userId || context?.user?.userId,
-          agencyId: account?.agencyId || context?.user?.loggedInAgencyId,
-          accessToken: context.accessToken,
-        })) || {};
-
-      const user = { ...userData, ...context?.user };
-
-      // Ensure a pair of email and name can be set
-      if (!((userName || user.name) && (userMail || user.mail))) {
-        return {
-          status: "ERROR_UNAUTHENTICATED_USER",
-        };
-      }
-
-      // Ensure user has municipalityAgencyId
-      if (!user.municipalityAgencyId) {
-        return {
-          status: "ERROR_MISSING_MUNICIPALITYAGENCYID",
-        };
-      }
-
-      // Fetch list of digitalAccess subscribers
-      const digitalAccessSubscriptions = await context.datasources
-        .getLoader("statsbiblioteketSubscribers")
-        .load("");
-
-      if (!digitalAccessSubscriptions[user.municipalityAgencyId]) {
-        return {
-          status: "ERROR_AGENCY_NOT_SUBSCRIBED",
-        };
-      }
-
-      // Pid must be a manifestation with a valid issn (valid journal)
-      let issn;
-      try {
-        const onlineAccess = await resolveAccess(pid, context);
-        issn = onlineAccess.find((entry) => entry.issn);
-      } catch (e) {
-        return {
-          status: "ERROR_PID_NOT_RESERVABLE",
-        };
-      }
-
-      if (!issn) {
-        return {
-          status: "ERROR_PID_NOT_RESERVABLE",
-        };
-      }
-
-      // Verify that the user is allowed to place an order
-      // checking the municipality exist is redundant - but we still want the blocked check.
-      const { status, statusCode } = await getUserBorrowerStatus(
-        { agencyId: user.municipalityAgencyId },
-        context
-      );
-
-      if (!status) {
-        return { ok: status, status: statusCode };
-      }
-
-      // Then send order
-      return await context.datasources
-        .getLoader("statsbiblioteketSubmitArticleOrder")
-        .load({
-          ...args.input,
-          userName: userName || user.name,
-          userMail: userMail || user.mail,
-          agencyId: user.municipalityAgencyId, //TODO - which agency should pay? BIBDK2021-1824
-          pickUpBranch: user.agency, //TODO shouldnt we get pickup branch from UI? BIBDK2021-1824
-          dryRun: args.dryRun,
-          originRequester,
-        });
+      return await placeCopyRequest(parent, args, context, info);
     },
   },
+};
+
+export const placeCopyRequest = async (parent, args, context, info) => {
+  const { pid, userName, userMail } = args.input;
+
+  // token is not authenticated
+  if (!context?.user?.userId) {
+    return {
+      status: "ERROR_UNAUTHENTICATED_USER",
+    };
+  }
+
+  const originRequester =
+    context?.smaug?.digitalArticleService?.originRequester;
+
+  if (!originRequester) {
+    return {
+      status: "ERROR_MISSING_CLIENT_CONFIGURATION",
+    };
+  }
+
+  // Basic user information (e.g. name, email)
+  const account = filterAgenciesByProps(context?.user?.agencies, {
+    type: "CPR",
+  })?.[0];
+
+  const userData =
+    (await context.datasources.getLoader("user").load({
+      userId: account?.userId || context?.user?.userId,
+      agencyId: account?.agencyId || context?.user?.loggedInAgencyId,
+      accessToken: context.accessToken,
+    })) || {};
+
+  const user = { ...userData, ...context?.user };
+
+  // Ensure a pair of email and name can be set
+  if (!((userName || user.name) && (userMail || user.mail))) {
+    return {
+      status: "ERROR_UNAUTHENTICATED_USER",
+    };
+  }
+
+  // Ensure user has municipalityAgencyId
+  if (!user.municipalityAgencyId) {
+    return {
+      status: "ERROR_MISSING_MUNICIPALITYAGENCYID",
+    };
+  }
+
+  // Fetch list of digitalAccess subscribers
+  const digitalAccessSubscriptions = await context.datasources
+    .getLoader("statsbiblioteketSubscribers")
+    .load("");
+
+  if (!digitalAccessSubscriptions[user.municipalityAgencyId]) {
+    return {
+      status: "ERROR_AGENCY_NOT_SUBSCRIBED",
+    };
+  }
+
+  // Pid must be a manifestation with a valid issn (valid journal)
+  let issn;
+  try {
+    const onlineAccess = await resolveAccess(pid, context);
+    issn = onlineAccess.find((entry) => entry.issn);
+  } catch (e) {
+    return {
+      status: "ERROR_PID_NOT_RESERVABLE",
+    };
+  }
+
+  if (!issn) {
+    return {
+      status: "ERROR_PID_NOT_RESERVABLE",
+    };
+  }
+
+  // Verify that the user is allowed to place an order
+  // checking the municipality exist is redundant - but we still want the blocked check.
+  const { status, statusCode } = await getUserBorrowerStatus(
+    { agencyId: user.municipalityAgencyId },
+    context
+  );
+
+  if (!status) {
+    return { ok: status, status: statusCode };
+  }
+
+  // Then send order
+  return await context.datasources
+    .getLoader("statsbiblioteketSubmitArticleOrder")
+    .load({
+      ...args.input,
+      userName: userName || user.name,
+      userMail: userMail || user.mail,
+      agencyId: user.municipalityAgencyId, //TODO - which agency should pay? BIBDK2021-1824
+      pickUpBranch: user.agency, //TODO shouldnt we get pickup branch from UI? BIBDK2021-1824
+      dryRun: args.dryRun,
+      originRequester,
+    });
 };
