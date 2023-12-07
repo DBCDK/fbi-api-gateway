14a38748376e76060fd668d1a8e12bef26eda863
kfudbc <kfudbc@users.noreply.github.com>
2023-12-07 09:41:12 +0100

    Stats collector (#268)
    * reimplement how timings are logged (now they should not be affected by busy event loop)
    
    * fix
    
    * include fastLane stats
    
    * docs
    
    * moved tracker to own file

--- src/datasourceLoader.js
+++ src/datasourceLoader.js
@@ -5,101 +5,7 @@ import { withRedis, clearRedis } from "./datasources/redis.datasource";
 import { createFetchWithConcurrencyLimit } from "./utils/fetchWithLimit";
 import { getFilesRecursive } from "./utils/utils";
 import config from "./config";
-
-/*
-  This is how we time parallel requests to a single datasource.
-  
-  Request A
-  ------------
-          Request B
-          ------------------
-                                      Request C
-                                      -----------
-  Total AB
-  --------------------------
-  
-  The totaltime for the requests should NOT be:
-  Request A + Request B + Request C
-  
-  But instead:
-  Total AB + Request C
-*/
-
-export const trackMe = () => {
-  return {
-    start: function (uuid) {
-      this.uuid = uuid;
-      return this;
-    },
-    createTracker: function (name) {
-      // If this datasource has not been tracked yet, initialize its tracking object.
-      if (!this.trackObject[name]) {
-        this.trackObject[name] = {
-          overlappingRequestCount: 0, // Number of ongoing requests for this datasource.
-          count: 0, // Total number of elements retrieved from datasource
-          started: 0, // Timestamp when the first concurrent request starts.
-          time: 0, // Total time taken for all requests so far.
-          sequentialTime: 0, // If requests were to be run sequentially, this is the total time
-          batches: 0, // Dataloader batches multiple requests, into a single request when possible
-        };
-      }
-    },
-
-    // Function to indicate the beginning of a datasource call.
-    begin: function (name, count) {
-      this.createTracker(name);
-
-      const beginTime = performance.now();
-
-      const trackObject = this.trackObject[name];
-
-      // Increment the count of ongoing requests.
-      trackObject.overlappingRequestCount++;
-
-      // Increment total number of elements retrieved from datasource
-      trackObject.count += count;
-      trackObject.batches++;
-
-      // Function to indicate the end of an datasource call.
-      return function end() {
-        const endTime = performance.now();
-        const duration = Math.round(endTime - beginTime);
-        // Decrement the count of ongoing requests.
-        trackObject.overlappingRequestCount--;
-
-        // If all ongoing requests are complete, calculate the time taken.
-        if (trackObject.overlappingRequestCount === 0) {
-          trackObject.time += duration;
-        }
-
-        trackObject.sequentialTime += duration;
-      };
-    },
-
-    cacheMiss: function (name, count) {
-      this.createTracker(name);
-
-      this.trackObject[name].cacheMiss =
-        (this.trackObject[name].cacheMiss || 0) + count;
-    },
-    uuid: null,
-    trackObject: {},
-    getMetrics: function () {
-      const res = {};
-
-      Object.entries(this.trackObject).forEach(([key, val]) => {
-        res[key] = {
-          count: val.count,
-          time: val.time,
-          cacheMiss: val.cacheMiss,
-          avg_ms: Math.round(val.sequentialTime / val.count),
-        };
-      });
-
-      return res;
-    },
-  };
-};
+import { createTracker } from "./utils/tracker";
 
 // Find all datasources in src/datasources
 export const datasources = getFilesRecursive(`${__dirname}/datasources`)
@@ -164,7 +70,7 @@ function setupDataloader(
   }
 
   async function batchLoaderWithTiming(keys) {
-    const end = context?.track?.begin?.(name, keys.length);
+    context?.stats?.incrementCount(name, keys.length);
 
     try {
       return await batchLoaderWithContext(keys);
@@ -180,8 +86,6 @@ function setupDataloader(
         err.stack += currentStack.stack.replace("Error", "\nBefore DataLoader");
       }
       throw err;
-    } finally {
-      end?.();
     }
   }
 
@@ -189,7 +93,7 @@ function setupDataloader(
     // If key is an object, we stringify
     // to make it useful as a cache key
     cacheKeyFn: (key) => (typeof key === "object" ? JSON.stringify(key) : key),
-    maxBatchSize: 100,
+    maxBatchSize: 10,
   });
 
   if (options?.redis?.prefix) {
@@ -207,11 +111,12 @@ function setupDataloader(
  */
 export default function createDataLoaders(uuid, testUser, accessToken) {
   const result = {};
-  const track = trackMe().start(uuid);
+  const stats = createTracker(uuid);
+  result.stats = stats;
 
-  result.trackingObject = track;
   const fetchWithConcurrencyLimit = createFetchWithConcurrencyLimit(
-    config.fetchConcurrencyLimit
+    config.fetchConcurrencyLimit,
+    stats
   );
   // Gets a loader by name.
   // A loader will be initialized first time it is called
@@ -219,11 +124,11 @@ export default function createDataLoaders(uuid, testUser, accessToken) {
     if (!result[name]) {
       result[name] = setupDataloader(nameToDatasource[name], {
         getLoader,
-        track,
         fetch: (url, options) => fetchWithConcurrencyLimit(url, options, name),
         trackingId: uuid,
         testUser,
         accessToken,
+        stats,
       })?.loader;
     }
     return result[name];
---
+++ src/utils/tracker.js
@@ -0,0 +1,113 @@
+/**
+ *
+ */
+function createTrackEntry() {
+  return {
+    count: 0,
+    redisHits: 0,
+    redisLookups: 0,
+    jsonParseSum: 0,
+    jsonStringifySum: 0,
+    waitingForServerResponseSum: 0,
+    contentDownloadSum: 0,
+    connectionStartSum: 0,
+    totalSum: 0,
+    bytesSum: 0,
+  };
+}
+
+/**
+ * Tracker for collecting stats for datasources, divided into HTTP requests,
+ * Redis requests, and JSON processing
+ */
+export function createTracker(uuid) {
+  const trackerObj = {
+    overall: createTrackEntry(),
+    datasources: {},
+  };
+
+  return {
+    addHTTP: (key, obj) => addHTTP(key, obj, trackerObj),
+    addRedisSet: (key, obj) => addRedisSet(key, obj, trackerObj),
+    addRedisGet: (key, obj) => addRedisGet(key, obj, trackerObj),
+    incrementCount: (key, count) => incrementCount(key, count, trackerObj),
+    incrementRedisHits: (key, count) =>
+      incrementRedisHits(key, count, trackerObj),
+    incrementRedisLookups: (key, count) =>
+      incrementRedisLookups(key, count, trackerObj),
+    summary: () => summary(trackerObj),
+    uuid,
+  };
+}
+
+function summary(trackerObj) {
+  const datasources = {};
+  Object.entries(trackerObj.datasources).forEach(
+    ([datasourceName, timings]) => {
+      datasources[datasourceName] = {
+        count: timings.count,
+        bytesIn: timings.bytesSum,
+        avgItemFetchMs: timings.totalSum / timings.count,
+        cacheMiss: timings.redisLookups - timings.redisHits,
+        cacheLookups: timings.redisLookups,
+        jsonProcessingMs: timings.jsonParseSum + timings.jsonStringifySum,
+      };
+    }
+  );
+
+  datasources.overall = {
+    count: trackerObj.overall.count,
+    cacheMiss: trackerObj.overall.redisLookups - trackerObj.overall.redisHits,
+    cacheLookups: trackerObj.overall.redisLookups,
+    jsonParseMs: trackerObj.overall.jsonParseSum,
+    jsonStringifyMs: trackerObj.overall.jsonStringifySum,
+    jsonProcessingMs:
+      trackerObj.overall.jsonParseSum + trackerObj.overall.jsonStringifySum,
+    avgItemFetchMs: trackerObj.overall.totalSum / trackerObj.overall.count,
+    bytesIn: trackerObj.overall.bytesSum,
+  };
+
+  return datasources;
+}
+
+function createEntry(key, trackerObj) {
+  if (!trackerObj.datasources[key]) {
+    trackerObj.datasources[key] = createTrackEntry();
+  }
+}
+function sumTimings(key, obj, trackerObj) {
+  Object.entries(obj).forEach(([field, val]) => {
+    const fieldSumName = field + "Sum";
+    if (typeof trackerObj.overall[fieldSumName] === "number") {
+      trackerObj.overall[fieldSumName] += val || 0;
+      trackerObj.datasources[key][fieldSumName] += val || 0;
+    }
+  });
+}
+function addHTTP(key, obj, trackerObj) {
+  createEntry(key, trackerObj);
+  sumTimings(key, obj, trackerObj);
+}
+function addRedisSet(key, obj, trackerObj) {
+  createEntry(key, trackerObj);
+  sumTimings(key, obj, trackerObj);
+}
+function addRedisGet(key, obj, trackerObj) {
+  createEntry(key, trackerObj);
+  sumTimings(key, obj, trackerObj);
+}
+function incrementCount(key, count, trackerObj) {
+  createEntry(key, trackerObj);
+  trackerObj.overall.count += count;
+  trackerObj.datasources[key].count += count;
+}
+function incrementRedisHits(key, count, trackerObj) {
+  createEntry(key, trackerObj);
+  trackerObj.overall.redisHits += count;
+  trackerObj.datasources[key].redisHits += count;
+}
+function incrementRedisLookups(key, count, trackerObj) {
+  createEntry(key, trackerObj);
+  trackerObj.overall.redisLookups += count;
+  trackerObj.datasources[key].redisLookups += count;
+}
