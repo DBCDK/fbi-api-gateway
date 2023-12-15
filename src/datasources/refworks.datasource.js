8b2226b224a2a9cd24942984e3392205c445a50f
pjohans <pjohans@gmail.com>
2023-12-15 10:17:54 +0100

    better regexp + function

--- src/datasources/refworks.datasource.js
+++ src/datasources/refworks.datasource.js
@@ -3,6 +3,7 @@ import config from "../config";
 const { url, ttl } = config.datasources.openformat;
 
 function parseResponse(response) {
+  // @TODO check for errors
   return splitResponse(response?.formatResponse?.refWorks[0]?.$);
 }
 
@@ -10,12 +11,13 @@ function splitResponse(ref) {
   // The first tag in refwork will always be the RT tag which determines what RefWorks record type to use.
   // if asked for multiple records openformat doesn't add a linebreak before the RT tag which is needed
   // in the refwork tool to understand it. We insert a linebreak here.
-  const regexp = /^RT .*/gm;
-  const matches = ref.match(regexp);
+  // const regexp = /^RT .*/gm;
+  const regexp = /(^RT .*\b)(?![\s\S]*\b\1\b)/gm;
+  // const matches = ref.match(regexp);
 
-  matches?.forEach((match) => (ref = ref.replaceAll(match, "\n" + match)));
-
-  return ref;
+  return ref.replace(regexp, function (match) {
+    return "\n" + match;
+  });
 }
 
 export async function load({ pids }, context) {
