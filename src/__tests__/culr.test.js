aa606b786846524070ac00034d824e88a57c9525
mtni <mtni@dbc.dk>
2024-01-16 20:58:35 +0100

    ...

--- src/__tests__/culr.test.js
+++ src/__tests__/culr.test.js
@@ -141,7 +141,7 @@ describe("Culr", () => {
     });
   });
 
-  it.only("CreateAccount | Should give status ERROR_INVALID_AGENCY", async () => {
+  it("CreateAccount | Should give status ERROR_INVALID_AGENCY", async () => {
     const result = await performTestQuery({
       query: createAccount,
       variables: {
