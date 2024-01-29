c98b5a8a177cb305b419c25257cabc3f231ee24c
mtni <mtni@dbc.dk>
2024-01-29 15:14:14 +0100

    fixed availability check

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -658,8 +658,8 @@ function handleLocalizationsWithKglBibliotek(
 
   const aggregatedAvailability = localizationsWithHoldingsNotKglBibliotek.sort(
     (a, b) => {
-      const aAvail = a.availability;
-      const bAvail = b.availability;
+      const aAvail = a?.availability;
+      const bAvail = b?.availability;
 
       return (
         Number(bAvail === "NOW") - Number(aAvail === "NOW") ||
@@ -668,7 +668,7 @@ function handleLocalizationsWithKglBibliotek(
         0
       );
     }
-  )?.[0].availability;
+  )?.[0]?.availability;
 
   const aggregateKglBibliotek =
     localizationsWithHoldingsIsKglBibliotek.length > 0
@@ -786,7 +786,7 @@ export async function resolveLocalizationsWithHoldings({
         bibdkExcludeBranches: bibdkExcludeBranches ?? false,
       });
 
-      const availability = library.availability;
+      const availability = library?.availability;
 
       return {
         hitcount: res.hitcount,
