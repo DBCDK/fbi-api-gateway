e45ff366e488a8638f271a72eb0136beceb81243
Kasper Fuglsang <kfu@dbc.dk>
2024-02-20 22:23:27 +0100

    Dfix expires_at

--- testuser-website/src/pages/api/oauth/token.js
+++ testuser-website/src/pages/api/oauth/token.js
@@ -1,7 +1,9 @@
 export default function handler(req, res) {
   const { code } = req.body;
   const decoded = Buffer.from(code, "base64").toString();
-  res
-    .status(200)
-    .json({ access_token: decoded, expires_at: Number.MAX_SAFE_INTEGER });
+
+  res.status(200).json({
+    access_token: decoded,
+    expires_at: Date.now() / 1000 + 60 * 60 * 24 * 30,
+  });
 }
