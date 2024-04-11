b99d9f71add830779ca6b514c8c59bd4c202ea70
pjohans <pjohans@gmail.com>
2024-04-11 16:26:01 +0200

    added snapshot + test

---
+++ src/__tests__/moodMatchSearch.test.js
@@ -0,0 +1,33 @@
+import { createMockedDataLoaders } from "../datasourceLoader";
+import { performTestQuery } from "../utils/utils";
+
+test("moodMatchSearch", async () => {
+  const result = await performTestQuery({
+    query: `
+        query ($q: String!, $limit: PaginationLimit!, $offset: Int!) {
+        moodSearch(q: $q) {
+          works(offset: $offset, limit: $limit) {
+            workId
+            titles {
+              main
+            }
+          }
+        }
+      }
+        `,
+    variables: {
+      q: "fisk",
+      limit: 10,
+      offset: 0,
+    },
+    context: {
+      profile: { agency: "190101", name: "bibdk21" },
+      smaug: {},
+      user: { userId: "fisk", loggedInAgencyId: "190101" },
+      accessToken: "DUMMY_TOKEN",
+      datasources: createMockedDataLoaders(),
+    },
+  });
+
+  expect(result).toMatchSnapshot();
+});
---
+++ src/datasources/mocked/moodMatchSearch.datasource.mocked.js
@@ -0,0 +1,60 @@
+// 20240411155803
+// http://mood-match-search-1-0.mi-prod.svc.cloud.dbc.dk/search?q=uhyggelig&field=title&debug=true
+export async function load({
+  q,
+  field,
+  offset = 0,
+  limit,
+  agency,
+  profile,
+  debug = false,
+}) {
+  console.log("MOODMATCHMOCKE");
+
+  const mock = {
+    responseHeader: {
+      time: 14.062,
+    },
+    response: [
+      {
+        workid: "work-of:870970-basis:51863437",
+        title: ["Uhyggelig sommer"],
+        creator: ["Vibeke Marx"],
+        abstract: [
+          "Krimi. Tre noveller om tasketyveri, mord og overfald. Lasse har ingen penge, så han overfalder en gammel dame. Zander bliver mistænkt for et mord på en pige, han har mødt på stranden og Lones mormor er blevet overfaldet og ligger i koma.",
+        ],
+      },
+      {
+        workid: "work-of:870970-basis:05681332",
+        title: ["Uhyggelig håndgribeligt Mad"],
+        creator: ["Al Jaffee"],
+        abstract: ["Sort/hvide tegneserieepisoder."],
+      },
+      {
+        workid: "work-of:870970-basis:06753035",
+        title: ["Ti uhyggelige fortællinger"],
+        creator: ["Bent Waagepetersen"],
+        abstract: [
+          "Noveller om spøgelser og genfærd, overnaturlige begivenheder og handlinger, der udspringer af irrationelle lag i menneskesindet. Foregår dels i Danmark, dels i Italien.",
+        ],
+      },
+      {
+        workid: "work-of:870970-basis:47929814",
+        title: ["Max Jordan - den uhyggelige dobbeltgænger"],
+        creator: ["Maurice Tillieux"],
+        abstract: [
+          "Tegneserie. Lumske robotter og en mystisk LP-plade, der ikke er, hvad den giver sig ud for at være, er blot nogle af de sjove og spændende ting, privatedetektiven Max Jordan bliver udsat for i disse 7 historier.",
+        ],
+      },
+      {
+        workid: "work-of:870970-basis:21400009",
+        title: ["Den grønne mil. 4. del : En uhyggelig død"],
+        creator: ["Stephen King", "Ole Varde Lassen"],
+        abstract: [
+          "Fangevogteren Percy har i simpel ondskab trådt Eduard Delacroix' mus ihjel - og det er ham der skal forestå henrettelsen af Eduard.",
+        ],
+      },
+    ],
+  };
+  return mock;
+}
