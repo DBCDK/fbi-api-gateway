22ef2b97d5f95d32d6d87259a5feb5bf66ae485b
kfudbc <kfudbc@users.noreply.github.com>
2024-04-30 11:06:52 +0200

    added field suggestTypes to suggester (#350)

--- src/datasources/suggester.datasource.js
+++ src/datasources/suggester.datasource.js
@@ -13,22 +13,26 @@ export async function load(
     workType = null,
     // suggestType defaults to all
     suggestType = "all",
+    suggestTypes,
     unique_works = true,
     limit,
     profile,
   },
   context
 ) {
-  const res = await context.fetch(
-    `${url}?${new URLSearchParams({
-      q: q,
-      ...(workType && { worktype: workType.toLowerCase() }),
-      unique_works,
-      type: suggestType.toLowerCase(),
-      limit: limit || 10,
-      profile: profile.name,
-    })}`
+  const selectedSuggestTypes = suggestTypes ? suggestTypes : [suggestType];
+  const searchParams = new URLSearchParams({
+    q: q,
+    ...(workType && { worktype: workType.toLowerCase() }),
+    unique_works,
+    limit: limit || 10,
+    profile: profile.name,
+  });
+  selectedSuggestTypes.forEach((type) =>
+    searchParams.append("type", type.toLowerCase())
   );
+
+  const res = await context.fetch(`${url}?${searchParams}`);
 
   const body = res.body;
 
