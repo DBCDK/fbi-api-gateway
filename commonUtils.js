f29475b9ca4e9f6a8ff6561f1dbfba6eac4b4f83
kfudbc <kfudbc@users.noreply.github.com>
2024-05-22 12:13:56 +0200

    fix roles (#361)
    * fix roles
    
    * rm async

--- commonUtils.js
+++ commonUtils.js
@@ -1,6 +1,13 @@
 import permissions from "./src/permissions.json";
 export function parseClientPermissions({ smaug }) {
-  return smaug?.gateway?.role
-    ? permissions[smaug?.gateway?.role]
-    : smaug?.gateway || permissions.default;
+  if (smaug?.gateway?.role && permissions[smaug?.gateway?.role]) {
+    return permissions[smaug?.gateway?.role];
+  }
+  if (smaug?.gateway?.admin) {
+    return { admin: true };
+  }
+  if (smaug?.gateway?.allowRootFields) {
+    return smaug?.gateway;
+  }
+  return permissions.default;
 }
