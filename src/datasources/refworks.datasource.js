e6792f0c13c08cb615418bbbb4393dee1fd80587
pjohans <pjohans@gmail.com>
2024-05-22 07:50:28 +0200

    in progress - refworks

--- src/datasources/refworks.datasource.js
+++ src/datasources/refworks.datasource.js
@@ -2,36 +2,55 @@ import config from "../config";
 
 const { url, ttl } = config.datasources.openformat;
 
-function parseResponse(response) {
-  // @TODO check for errors
-  return splitResponse(response?.formatResponse?.refWorks[0]?.$);
+async function postObject(pids) {
+  const repositoryIds = pids.map((pid) => ({
+    repositoryId: pid,
+  }));
+
+  return {
+    formats: [
+      {
+        name: "refWorks",
+        mediaType: "text/plain",
+      },
+    ],
+    objects: repositoryIds,
+    trackingId: "some-uuid",
+  };
 }
 
-function splitResponse(ref) {
-  // The first tag in refwork will always be the RT tag which determines what RefWorks record type to use.
-  // if asked for multiple records openformat doesn't add a linebreak before the RT tag which is needed
-  // in the refwork tool to understand it. We insert a linebreak here.
-  const regexp = /^RT .*/gm;
-  return ref.replace(regexp, function (match) {
-    return "\n" + match;
-  });
+export function parseResponse(response) {
+  console.log(JSON.stringify(response, null, 4), "REEEEEEEEEEEEEEEEEESPONSE");
+
+  const refWorksArray = response?.body?.objects?.map(
+    (obj) => obj?.refWorks?.[0]?.formatted
+  );
+
+  console.log(
+    JSON.stringify(refWorksArray, null, 4),
+    "ARRRRRRRRRRRRAUYYYYYYYYYY"
+  );
+
+  return refWorksArray.join("\n");
 }
 
 export async function load({ pids }, context) {
-  const response = (
-    await context?.fetch(
-      `${url}?action=formatObject&pid=${pids.join(
-        ","
-      )}&outputFormat=refWorks&outputType=json`
-    )
-  ).body;
+  const params = await postObject(pids);
+  const response = await context.fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(params),
+  });
+
   return parseResponse(response);
 }
 
-export const options = {
-  redis: {
-    prefix: "refworks-1",
-    ttl,
-    staleWhileRevalidate: 60 * 60 * 24 * 30, // 30 days
-  },
-};
+// export const options = {
+//   redis: {
+//     prefix: "refworks-1",
+//     ttl,
+//     staleWhileRevalidate: 60 * 60 * 24 * 30, // 30 days
+//   },
+// };
