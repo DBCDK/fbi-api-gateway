78078c2c0cf6d9d756b8d6dfc91e9e01571dc21b
Kasper Fuglsang <kfu@dbc.dk>
2024-06-11 11:47:16 +0200

    do not download drupal schema at runtime

--- src/schema/external/drupal.js
+++ src/schema/external/drupal.js
@@ -2,6 +2,7 @@ import request from "superagent";
 import { introspectSchema, wrapSchema, RenameTypes } from "@graphql-tools/wrap";
 import { print } from "graphql";
 import config from "../../config";
+import drupalSchema from "./drupalSchema.json";
 
 const executor = async ({ document, variables }) => {
   const query = print(document);
@@ -18,10 +19,22 @@ const typeNameMap = {
 
 export default async () => {
   const executableSchema = wrapSchema({
-    schema: await introspectSchema(executor),
+    schema: await introspectSchema(() => drupalSchema),
     executor,
     transforms: [new RenameTypes((name) => typeNameMap[name] || name)],
   });
 
   return executableSchema;
 };
+
+// Latest schema can be downloaded by uncommenting this
+// (async () => {
+//   await introspectSchema(async (...args) => {
+//     const drupalSchema = await executor(...args);
+//     require("fs").writeFileSync(
+//       require("path").join(__dirname, "drupalSchema.json"),
+//       JSON.stringify(drupalSchema, null, 2)
+//     );
+//     return drupalSchema;
+//   });
+// })();
