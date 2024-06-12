17fc8a6703c497cc1451db0184e2fd5070944980
kfudbc <kfudbc@users.noreply.github.com>
2024-06-12 08:06:42 +0200

    added http timeouts for calls to datasources (#381)

--- src/config.js
+++ src/config.js
@@ -13,6 +13,7 @@ export default {
   },
   // How many outgoing HTTP requests a single incoming request can make in parallel
   fetchConcurrencyLimit: process.env.FETCH_CONCURRENCY_LIMIT || 10,
+  fetchDefaultTimeoutMs: process.env.FETCH_DEFAULT_TIMEOUT_MS || 10000,
   datasources: {
     ocn2pid: {
       url:
