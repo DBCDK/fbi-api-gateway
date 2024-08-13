9df0c9cc72d33a222e1db69a5d8aa95ac199ee41
pjohans <pjohans@gmail.com>
2024-08-13 14:01:03 +0200

    physicaldescription is NOT required

--- src/schema/draft/manifestation.js
+++ src/schema/draft/manifestation.js
@@ -732,7 +732,7 @@ type Manifestation {
   """
   Physical description  of this manifestation like extent (pages/minutes), illustrations etc.
   """
-  physicalDescription: PhysicalUnitDescription! 
+  physicalDescription: PhysicalUnitDescription 
  
 
   """
