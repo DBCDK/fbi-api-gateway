c3d451d727a727e40f07d5e13f4f131d146deb64
kfudbc <kfudbc@users.noreply.github.com>
2024-09-18 11:08:50 +0200

    added openagency to fbi-api (#412)
    * added openagency to fbi-api
    
    * fix
    
    * fix permissions file
    
    * modify schema a bit
    
    * fix vip example
    
    * allow 404 from vip
    
    * rm old permissions.json

---
+++ src/datasources/vip/vipsearchprofiles.datasource.js
@@ -0,0 +1,26 @@
+import config from "../../config";
+
+const { url, ttl, prefix } = config.datasources.vipcore;
+
+/**
+ * Search for vip opensearch profiles using vip-core->opensearchprofile
+ */
+export async function load({ agencyId, profileName }, context) {
+  const res = await context.fetch(url + "/opensearchprofile", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({ agencyId, profileName, profileVersion: 3 }),
+    allowedErrorStatusCodes: [404],
+  });
+
+  return res?.body;
+}
+
+export const options = {
+  redis: {
+    prefix: prefix + "_searchprofiles",
+    ttl,
+  },
+};
