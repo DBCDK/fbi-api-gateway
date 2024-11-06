6092339fffc5654dde257e4ced52901133a111a1
Kasper Røen Fuglsang <kfu@dbc.dk>
2024-11-06 08:51:43 +0100

    adding bibevents

---
+++ src/datasources/bibevents/bibevents.datasource.js
@@ -0,0 +1,31 @@
+import config from "../../config";
+
+const { url, prefix } = config.datasources.bibevents;
+
+export async function load(args, context) {
+  let argsCopy = { ...args };
+  argsCopy.offset = argsCopy.offset || 0;
+  argsCopy.limit =
+    argsCopy.limit < 0 || argsCopy.limit > 100 ? 10 : argsCopy.limit;
+  let queryParams = "";
+  Object.entries(args || {}).forEach(([key, valArr]) => {
+    const delimiter = queryParams.length > 0 ? "&" : "?";
+    if (Array.isArray(valArr)) {
+      valArr.forEach((v) => {
+        queryParams += delimiter + key + "=" + encodeURIComponent(v);
+      });
+    } else {
+      queryParams += delimiter + key + "=" + encodeURIComponent(valArr);
+    }
+  });
+  console.log(`${url}/api/events${queryParams}`);
+  const res = await context.fetch(`${url}/api/events${queryParams}`);
+  return res;
+}
+
+export const options = {
+  redis: {
+    prefix: "bibevents-" + prefix,
+    ttl: 60 * 5,
+  },
+};
