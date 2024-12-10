148b2f8099a54c12c2e613b812253c9d27c97dfa
pjohans <pjohans@gmail.com>
2024-12-10 12:54:35 +0100

    added a mocked datasource - fixed a test

--- src/datasources/mocked/seriesService/identifyWork.datasource.mocked.js
+++ src/datasources/mocked/seriesService/identifyWork.datasource.mocked.js
@@ -5,7 +5,17 @@
  */
 export async function load({ workId }) {
   const mock = {
-    series: [{ id: "1", numberInSeries: "1", title: "De syv søstre-serien" }],
+    trackingId: "1362f3da-5b7e-40fb-be6e-bd4f13735f6d",
+    series: [
+      {
+        id: "52484af11e5beceb6340880eb9e325fa216cee2fb68c8d4f0d76029a7e255fff",
+        title: "De syv søstre-serien",
+      },
+    ],
   };
+
+  //   {
+  //   series: [{ id: "1", numberInSeries: "1", title: "De syv søstre-serien" }],
+  // };
   return mock;
 }
