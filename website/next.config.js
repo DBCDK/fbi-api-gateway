1f9a1d8874c5f9653c988b5716e99b549583cadc
mtni <mtni@dbc.dk>
2025-01-23 12:30:13 +0100

    ...

--- website/next.config.js
+++ website/next.config.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 module.exports = {
   reactStrictMode: true,
   i18n: {
@@ -15,5 +17,16 @@ module.exports = {
         permanent: false,
       },
     ];
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      
+      // The default imports used by Voyager GraphQL may cause errors. These can be resolved by overriding the import paths to point to the root directory.
+      "graphql/execution": path.resolve(__dirname, "node_modules/graphql"),
+      "graphql/type": path.resolve(__dirname, "node_modules/graphql"),
+      "graphql/utilities": path.resolve(__dirname, "node_modules/graphql")
+    };
+    return config;
   },
 };
