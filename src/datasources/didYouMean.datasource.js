8bd3f9b5722b8308e090883e05d42ea4f8173040
Yousef Mohsen <yousefmohsen@outlook.dk>
2025-02-10 09:23:07 +0100

    more clean up

--- src/datasources/didYouMean.datasource.js
+++ src/datasources/didYouMean.datasource.js
@@ -1,9 +1,8 @@
 import config from "../config";
 
-const { teamLabel } = config.datasources.didyoumean;
+const { teamLabel,url } = config.datasources.didyoumean;
 
 export async function load({ q, limit = 5 }, context) {
-  const { url, teamLabel } = config.datasources.didyoumean;
 
   const result = await context.fetch(
     `${url}/did-you-mean?q=${q?.all}&limit=${limit}`
