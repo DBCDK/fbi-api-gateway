0b2c49fce66e7f7cd3a65a3c6d18c4f462184094
Kasper Røen Fuglsang <kfu@dbc.dk>
2025-02-20 14:41:02 +0100

    clean up

--- src/datasources/mocked/fbiinfoCovers.datasource.mocked.js
+++ src/datasources/mocked/fbiinfoCovers.datasource.mocked.js
@@ -13,6 +13,12 @@ export async function load(pid) {
         "240px": { url: "link-to-240px", width: 240, height: 800 },
         "120px": { url: "link-to-120px", width: 120, height: 800 },
       },
+      moreinfo_working_pid: {
+        "480px": { url: "link-to-480px", width: 480, height: 800 },
+        "960px": { url: "link-to-960px", width: 960, height: 800 },
+        "240px": { url: "link-to-240px", width: 240, height: 800 },
+        "120px": { url: "link-to-120px", width: 120, height: 800 },
+      },
     }[pid] || {}
   );
 }
