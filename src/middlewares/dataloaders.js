419a243c83d36ce3ef62c2ab4ad71baa5221614f
Kasper Røen Fuglsang <kfu@dbc.dk>
2025-04-11 08:36:38 +0200

    allow debug via config

--- src/middlewares/dataloaders.js
+++ src/middlewares/dataloaders.js
@@ -1,6 +1,8 @@
 import { v4 as uuid } from "uuid";
-
+import config from "../config";
 import createDataLoaders from "../datasourceLoader";
+
+const allowDebug = config.allowDebug;
 
 /**
  * Inits dataloaders
@@ -12,7 +14,7 @@ export function initDataloaders(req, res, next) {
   // - Fetch requests and responses from datasources with `allowDebug: true` are collected.
   // - Redis caching is disabled for those datasources to ensure real-time responses.
   // - Collected debug data is returned in the GraphQL response under `extensions.debug`.
-  const debugEnabled = req.headers["x-debug"] === "true";
+  const debugEnabled = allowDebug && req.headers["x-debug"] === "true";
 
   // Allocate a debug object to collect request/response details, or null if not debugging
   req.datasourcesDebug = debugEnabled ? {} : null;
