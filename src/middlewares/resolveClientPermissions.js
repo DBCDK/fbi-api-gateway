b9f735f04b1600e424ad893b2aa700eaa9cb6c7b
mtni <mtni@dbc.dk>
2025-11-10 19:37:59 +0100

    changed description

--- src/middlewares/resolveClientPermissions.js
+++ src/middlewares/resolveClientPermissions.js
@@ -1,7 +1,7 @@
 import { parseClientPermissions } from "../../commonUtils";
 
 /**
- * Resolves the GraphQL query
+ * Adds the client permissions to context object
  */
 export async function resolveClientPermissions(req, res, next) {
   const parsedPermissions = parseClientPermissions({
