11988e5e5e321e9f5f6218111be6a4768ccdfd28
Lars Krogsgård Hansen <lkh@dbc.dk>
2025-11-27 08:55:42 +0100

    NET-1707: Close an order on behalf of an user
    Our librarians must be able to close an order on behalf of an enduser. They are only allowed to do so within their own agency.

--- src/permissions.js
+++ src/permissions.js
@@ -65,6 +65,7 @@ const bibdk = {
     "user",
     // mutation
     "submitOrder",
+    "closeOrder",
     "submitMultipleOrders",
     "data_collect",
     "deleteOrder",
---
+++ src/schema/netpunkt/close.js
@@ -0,0 +1,65 @@
+export const typeDef = `
+
+  input NetpunktCloseOrderInput {
+    requesterId: String
+    orderId: String
+  }
+  type Netpunkt {
+    """
+    Closes an order from Netpunkt to OpenOrder
+    """
+    closeOrder(input: NetpunktCloseOrderInput!, dryRun: Boolean): CloseOrder
+  }
+  extend type Mutation {
+    """
+    Netpunkt related mutations
+    """
+    netpunkt: Netpunkt!
+  }
+  
+  type CloseOrder {
+    orderId: String,
+   }
+`;
+
+export const resolvers = {
+  Mutation: {
+    netpunkt() {
+      return {};
+    },
+  },
+  Netpunkt: {
+    async closeOrder(parent, args, context) {
+      // The userId of the Netpunkt user (not the end user)
+      const authUser = context?.user;
+
+      if (args?.dryRun) {
+        return { orderId: "654321" };
+      }
+      if (args?.input?.requesterId !== authUser.netpunktAgency) {
+        return {
+          status: "REQUESTERID NOT EQUAL TO LOGIN ID",
+        };
+      }
+
+      const branch = (
+        await context.datasources.getLoader("library").load({
+          branchId: args?.input?.requesterId,
+        })
+      ).result?.[0];
+
+      if (!branch) {
+        return {
+          status: "UNKNOWN_REQUESTERID",
+        };
+      }
+
+      //TODO: CHECK librarians agencyId compared to requesterId.
+      return await context.datasources.getLoader("closeOrder").load({
+        accessToken: context.accessToken,
+        orderId: args.input?.orderId,
+        requesterId: args.input?.requesterId
+      });
+    },
+  },
+};
