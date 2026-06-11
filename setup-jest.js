5ae116567d459d38b1cc4debeadff28bc9fff387
Kasper Røen Fuglsang <kfu@dbc.dk>
2026-06-11 13:06:12 +0200

    audit fix

--- setup-jest.js
+++ setup-jest.js
@@ -8,9 +8,14 @@ jest.mock("./src/utils/fetchWorker", () => ({
 }));
 
 jest.mock("fast-jwt", () => ({
-  __esModule: true, // this property makes it work
-  default: "mockedDefaultExport",
-  createSigner: jest.fn(),
+  __esModule: true,
+  createSigner: () => (payload) => `mock-jwt-${JSON.stringify(payload)}`,
+}));
+
+jest.mock("uuid", () => ({
+  __esModule: true,
+  v4: jest.fn(() => "00000000-0000-4000-8000-000000000000"),
+  v5: jest.fn(() => "00000000-0000-4000-8000-000000000001"),
 }));
 
 jest.mock("nanoid", () => ({
