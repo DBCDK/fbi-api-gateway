8b8eacdee1c165a53917262d7b8af3dc66c14bd8
Kasper Røen Fuglsang <kfu@dbc.dk>
2026-06-26 10:54:58 +0200

    fullText must not be in html format for infomedia articles

---
+++ src/utils/htmlToPlainText.js
@@ -0,0 +1,28 @@
+/**
+ * Convert HTML-ish content to plain text with sensible line breaks.
+ */
+export function htmlToPlainText(input) {
+  if (!input || typeof input !== "string") {
+    return input;
+  }
+
+  return input
+    .replace(/<(?:br|hr)\s*\/?>/gi, "\n")
+    .replace(/<\/(?:p|div|li|h[1-6]|tr|blockquote)>/gi, "\n")
+    .replace(/<[^>]+>/g, "")
+    .replace(/&nbsp;/gi, " ")
+    .replace(/&#(\d+);/g, (_, code) => String.fromCharCode(Number(code)))
+    .replace(/&#x([0-9a-f]+);/gi, (_, hex) =>
+      String.fromCharCode(parseInt(hex, 16))
+    )
+    .replace(/&lt;/g, "<")
+    .replace(/&gt;/g, ">")
+    .replace(/&quot;/g, '"')
+    .replace(/&#39;/g, "'")
+    .replace(/&apos;/g, "'")
+    .replace(/&amp;/g, "&")
+    .replace(/\r\n/g, "\n")
+    .replace(/[^\S\n]+/g, " ")
+    .replace(/\n{3,}/g, "\n\n")
+    .trim();
+}
